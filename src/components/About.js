6be2f6a71572f82d147fe106a3269e3dfb83041e
Phoebe Torchia <phoebetorchia@Phoebes-Air.fios-router.home>
2021-12-07 20:22:37 -0500

    updated text colors:

--- src/components/About.js
+++ src/components/About.js
@@ -7,10 +7,10 @@ const About = () => (
 <p className="fs-5 fade-inHey"><b>hey there!</b> my name is</p><p id="myname" className="display-2 mx-5 text-start fade-inName"> phoebe,</p> 
       <p className="display-3 fade-inTitle">and i'm a: </p>
       <div className="fade-inInfo mx-auto mt-5 p-5">
-      <p>Full Stack <b>Web</b> + <b>Mobile</b> Engineer,</p>
+      <p>Full Stack <b className="purple">Web</b> + <b className="purple">Mobile</b> Engineer,</p>
       <p><b id="a">who recently finished a full time immersive Software Development Bootcamp:</b> <b id="gh"><a className="gh" href="https://www.gracehopper.com/curriculum">Grace Hopper @ Fullstack Academy</a></b>, in July 2021.</p>
       <p><b id="a">now</b> I'm currently based in Philly, but I love to travel, and I'm open to remote & freelance work.</p>
-      <p>Contact me, let's <b>connect</b>!</p>
+      <p>Contact me, let's <b className="purple">connect</b>!</p>
       </div>
     </div>
   </div>
