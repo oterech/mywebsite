deb71e683dee3b7bf245a44e89c96a9469dbcbd0
Phoebe Torchia <phoebetorchia@Phoebes-Air.fios-router.home>
2021-12-07 20:15:59 -0500

    updated card front and back

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,28 +2,28 @@
 // Flip Card for each Project, picture front, description back, links to project/git hub
 
 import React, { Component, useState } from "react";
-import images from "../images"
-import stack from "../assets/stack.png"
 
 export default function Projects() {
   return (
     <div>
     <h1 className="mt-5 pt-5 display-1 text-center projects">Projects</h1>
     <div className="p-5 d-flex mx-auto justify-content-center flex-wrap">
-      {images.map(({ id, src, title, description }) => {
+      {descriptions.map(({ id, src, title, description }) => {
         const cardInfo = (descriptions.filter(item => {
         return  item.id === id
         }))
         const data = cardInfo[0]
         return <div className="flip-card mx-1 my-1">
           <div className="flip-card-inner">
-            <div className="flip-card-front">
-            <img src={src} title={title} description={description} id={id} key={id}  />
+            <div className="flip-card-front d-flex justify-content-center">
+            <h1 className="my-auto mx-1">{title}</h1>
             </div>
-            <div className="flip-card-back border border-1">
-              <a href={data.link} className="title fs-4">{data.title}</a>
-              <p className="mt-1 dates">{data.dates}</p>
-              <p className="mx-1">{data.description}</p>
+            <div className="flip-card-back border d-flex p-1 border-1">
+              <div className="mt-2 align-content-center">
+              <a href={data.link} className="title position-absolute overflow-visible mx-1 mt-2 text-no-wrap fs-4">{data.title}</a>
+              <p className="mt-4 dates ps-2 pt-3 opacity-75 position-absolute">{data.dates}</p>
+              </div>
+              <p className="my-auto pt-2 mx-3">{data.description}</p>
             </div>
           </div>
         </div>
